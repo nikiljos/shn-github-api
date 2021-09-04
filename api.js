756fa856c56ff2d772253f93c6b28fd3e1aff073
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-09-04 20:40:00 +0530

    something working

--- api.js
+++ api.js
@@ -1,12 +1,15 @@
-getData();
-
 async function getData() {
+    document.getElementById("followers").innerHTML = ""
 
     details = await axios.get(`https://api.github.com/users/nikiljos/following`)
         .then((value, err) => {
-            return value.data[20]
+            return value.data
         })
 
     console.log(details)
+    n = details.length
+    for (i = 0; i < n; i++) {
+        document.getElementById("followers").innerHTML += `<div class='col-md-4'><div class='card mb-4 box-shadow'><div class='card-body'>${details[i].login}</div> </div> </div>`
 
+    }
 }
